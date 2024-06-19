0677c9869483876f9c9336a98f1ab02ecb677b70
waregin <warebec@gmail.com>
2024-06-19 11:14:48 -0400

    problem 37 solved and recorded

--- problem38.py
+++ problem38.py
@@ -3,25 +3,24 @@ from datetime import datetime
 
 print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')}")
 
-concatenations = []
+largest = 0
 
 for i in range(1, 10000):
     concatenated = ""
-    for j in range(1, 100):
+    for j in range(1, 10):
         p = i * j
         concatenated = concatenated + str(p)
         if len(concatenated) == 9:
-            add = True
+            is_pandigital = True
             for k in range(1, 10):
                 if not str(k) in concatenated:
-                    add = False
+                    is_pandigital = False
                     break
-            if add:
-                concatenations.append(concatenated)
+            if is_pandigital:
+                largest = max(int(concatenated), largest)
             break
         if len(concatenated) > 9:
             break
 
-print(concatenations)
-print(max(concatenations))
+print(largest)
 print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')}")
